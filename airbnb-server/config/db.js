735a0062ad33f6918a7be95e1019e523b73d4748
Alyessa Moodley <aly711@live.com>
2026-07-04 19:43:19 +0200

    updated airbnb-server/config/db.js so that when Node detects a local DNS proxy on 127.0.0.1, it falls back to public DNS servers first

--- airbnb-server/config/db.js
+++ airbnb-server/config/db.js
@@ -1,5 +1,16 @@
 // Import mongoose so we can connect to MongoDB
+const dns = require("dns");
 const mongoose = require("mongoose");
+
+// Local DNS proxies (127.0.0.1) often reject SRV lookups required by mongodb+srv URIs
+const dnsServers = dns.getServers();
+if (dnsServers.some((server) => server.startsWith("127."))) {
+  dns.setServers([
+    "8.8.8.8",
+    "1.1.1.1",
+    ...dnsServers.filter((server) => !server.startsWith("127.")),
+  ]);
+}
 
 // Function responsible for connecting to MongoDB
 const connectDB = async () => {
